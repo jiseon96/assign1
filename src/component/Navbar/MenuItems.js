2cee23414a50115d339f69a2fc21004237055838
jiseon96 <jamiejs9605@gmail.com>
2021-12-15 21:48:26 +0900

    assign1

---
+++ src/component/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+  {
+    title: "News",
+    url: "#news",
+    cName: "nav-links",
+  },
+  {
+    title: "Contact",
+    url: "#contact",
+    cName: "nav-links",
+  },
+  {
+    title: "About",
+    url: "#about",
+    cName: "nav-links",
+  },
+];
